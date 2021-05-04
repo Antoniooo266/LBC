846bc8793a531589a5a182d69486c5d1c1170f34
Antonio <Antonio@LAPTOP-Q1IEL971>
2021-05-04 14:09:07 +0200

    lkygjkhk

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'Mcedtpm1203',
     database: 'lbc'
 });
 
