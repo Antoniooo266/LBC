846f1841eceed1124e8cb8b33e769807cc1aefac
Antonio <Antonio@LAPTOP-Q1IEL971>
2021-05-07 13:32:25 +0200

    Usuario Master Done

--- backend/config.js
+++ backend/config.js
@@ -1,15 +1,13 @@
 const mysql = require('mysql');
-var fs = require('fs');
-var readline = require('readline');
 
 //----PARAMETROS PARA LA CONEXION A LA BASE DE DATOS----
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
-    user: 'root',
-    password: 'Mcedtpm1203',
-    database: '',
+    user: 'master',
+    password: '1234',
+    database: 'lbc',
     multipleStatements: true
 });
 
@@ -23,21 +21,5 @@ connection.connect(error =>{
 })
 
 //----END CONEXION A LA BASE DE DATOS----
-
-//----EXECUTE SQL BD----
-
-var rl = readline.createInterface({
-    input: fs.createReadStream('./scripts/Crear_BD.sql'),
-    terminal: false
-   });
-  rl.on('line', function(chunk){
-      connection.query(chunk.toString('ascii'), function(err, sets, fields){
-       if(err) console.log(err);
-      });
-  });
-  rl.on('close', function(){
-    console.log("finished");
-    connection.end();
-});
 
 module.exports = connection;
