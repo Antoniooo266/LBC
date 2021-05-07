cebc2f04d78122430e7672925d3224f9624a9d70
abeja21 <gestruiik@gmail.com>
2021-05-07 11:59:39 +0200

    mod y del torn

--- backend/torneosrouter.js
+++ backend/torneosrouter.js
@@ -36,55 +36,45 @@ router.get("/get", (req, res) => {
   });
 });
 
-//Add Resultados
-router.post("/addresult", async (req, res) => {
-  var equipoLocal;
-  var equipoVisitante;
 
-  GetIdEquipo(req.body.Visitante, function (err, data) {
-    if (err) {
-      console.log("ERROR : ", err);
-    } else {
-      equipoVisitante = data;
-    }
-  });
 
-  GetIdEquipo(req.body.Local, function (err, data) {
-    if (err) {
-      console.log("ERROR : ", err);
-    } else {
-      equipoLocal = data;
-    }
-  });
-  setTimeout(()=>{console.log(equipoLocal);
-    const resultObj = {
-      ID_Visitante: equipoVisitante,
-      ID_Local: equipoLocal,
-      Resultado_Local: req.body.ResultLocal,
-      Resultado_Visitante: req.body.ResultVisitante,
-      Fecha: req.body.Date,
-    };
-    console.log(resultObj);
-    connection.query("INSERT INTO partido SET ? ", resultObj, (error) => {
-      if (error) {
-        throw error;
-      } else {
-        res.redirect("/public/Mensaje.html");
-      }
-    });
-}, 3000);
-  
+//----Borrar Torneo----
+
+router.delete('/delete',(req,res)=>{
+connection.query('DELETE * FORM torneo WHERE ID_Torneo =?'[req.body.ID_Torneo],function (err,solution){
+  if (err) throw err;
+
+  res.redirect('../public/Mensaje.html')
+})
 });
 
-function GetIdEquipo(nombre, callback) {
-  connection.query(
-    "SELECT ID_Equipo FROM equipo WHERE Nombre = ?",
-    [nombre],
-    function (err, result) {
-      if (err) callback(err, null);
-      else callback(null, result[0].ID_Equipo);
-    }
-  );
-}
+//----Fin Borrar Torneo----
+
+//----Modificar Torneo----
+
+router.put('/update',(req,res)=>{
+  const torneoObj = {
+    Nombre: req.body.name,
+    ID_Juego: req.body.joggo,
+    Cantidad: req.body.teams,
+    Fecha: req.body.fecha,
+    Premio: req.body.premio,
+  };
+  
+  connection.query('UPDATE torneo SET ? WHERE ID_Torneo')[torneoObj,req.body.ID_Torneo],(err,res)=>{
+if (err) throw err;
+
+res.redirect('../public/Mensaje.html')
+  
+
+
+  }
+  
+
+  
+
+})
+
+//----Fin Modificar Torneo----
 
 module.exports = router;
