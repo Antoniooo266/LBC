cde473ff55179685a1e7194e709426be071c6382
Sergio Lorente <slorente@campuscamarafp.com>
2021-05-14 09:18:05 +0200

    retoques

--- backend/public/js/ActualizarRango.js
+++ backend/public/js/ActualizarRango.js
@@ -1,9 +1,9 @@
 const connection = require("../../config");
 
 function Actualizar(id_usuario){
-    var valorLista = document.getElementsByName("Rangos")
+    var valorLista = [id_usuario.selectedIndex].value
 
-    connection.query("UPDATE Rango FROM usuario WHERE ID_usuario = ? SET ?",[id_usuario,valorLista],function (err,res){
+    connection.query("UPDATE usuario SET Rango = ? WHERE ID_usuario = ?",[valorLista,id_usuario],function (err,res){
 if(err) throw err;
 
 console.log("niceee")
