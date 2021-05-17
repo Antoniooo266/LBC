4f85c34a42342eed60ad7d8bff151ac045c57a43
Antonio <Antonio@LAPTOP-Q1IEL971>
2021-05-17 09:45:19 +0200

    creacion carpeta views

---
+++ backend/views/view_tabla_perfil.sql
@@ -0,0 +1,13 @@
+use lbc;
+create definer = root@localhost view view_tabla_perfil as
+select `lbc`.`usuario`.`Nickname`  AS `Nickname`,
+       `lbc`.`usuario`.`Fecha_Nac` AS `Fecha_Nac`,
+       `lbc`.`usuario`.`Correo`    AS `Correo`,
+       `lbc`.`usuario`.`País`      AS `País`,
+       `lbc`.`rango`.`NombreRango` AS `NombreRango`,
+       `e`.`NombreEquipo`          AS `NombreEquipo`,
+       `e`.`Victorias`             AS `Victorias`,
+       `e`.`Derrotas`              AS `Derrotas`,
+       `e`.`Media`                 AS `Media`
+from ((`lbc`.`usuario` left join (`lbc`.`usuario_equipo` left join `lbc`.`equipo` `e` on ((`lbc`.`usuario_equipo`.`ID_Equipo` = `e`.`ID_Equipo`))) on ((`lbc`.`usuario`.`ID_Usuario` = `lbc`.`usuario_equipo`.`ID_Usuario`)))
+         left join `lbc`.`rango` on ((`lbc`.`usuario`.`Rango` = `lbc`.`rango`.`ID_Rango`)));
