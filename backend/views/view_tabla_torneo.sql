6bcd7a452d6a6813b049a0a91d48931f1c5e6b4d
Sergio Lorente <slorente@campuscamarafp.com>
2021-05-21 08:21:07 +0200

    cositias parte4

--- backend/views/view_tabla_torneo.sql
+++ backend/views/view_tabla_torneo.sql
@@ -1,6 +1,7 @@
 use lbc;
 create definer = root@localhost view view_tabla_torneo as
-select `lbc`.`torneo`.`NombreTorneo` AS `NombreTorneo`,
+select `lbc`.`torneo`.`ID_Torneo` AS `ID_Torneo`,
+        `lbc`.`torneo`.`NombreTorneo` AS `NombreTorneo`,
        `lbc`.`torneo`.`Fecha`        AS `Fecha`,
        `lbc`.`torneo`.`Premio`       AS `Premio`,
        `e`.`NombreEquipo`            AS `NombreEquipo`
