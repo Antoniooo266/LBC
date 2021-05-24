de646cee49164d5ff27b2643c81233cb8a938837
Antonio <Antonio@LAPTOP-Q1IEL971>
2021-05-24 09:24:37 +0200

    el que come callao repite

--- backend/equiporouter.js
+++ backend/equiporouter.js
@@ -15,4 +15,40 @@ router.get("/get", (req, res) => {
     });
   });
 
+router.post('/add', (req, res) =>{
+  const EquipoObj = {
+      NombreEquipo: req.body.name,
+      Fecha: req.body.date
+  };
+  connection.query('INSERT INTO equipo SET ?', EquipoObj, (error) =>{
+    if (error){
+        throw error;
+    }else{
+        res.redirect('/public/Mensaje.html')
+    }
+  })
+});
+
+router.post('/delete',(req,res)=>{ 
+  connection.query('DELETE FROM torneo WHERE ID_Equipo = ?',[req.body.Eliminar],function (err,solution){
+    if(err) throw err;
+    res.redirect('/public/Mensaje.html')
+  })
+});
+
+router.post('/update',(req,res)=>{
+  const NewEquipo = {
+    ID_Equipo: req.body.id,
+    NombreEquipo: req.body.name,
+    Fecha: req.body.date
+  }
+  console.log(NewTorneo);
+  connection.query('UPDATE torneo SET NombreTorneo = ?, Fecha = ? WHERE ID_Equipo = ?',[NewEquipo.NombreEquipo, NewEquipo.Fecha, NewEquipo.ID_Equipo],function (err,solution){
+    if(err) throw err;
+    res.redirect('/public/Mensaje.html');
+  })
+  });
+
+
+
   module.exports = router;
