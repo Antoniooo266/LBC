526b9e78f219d7e411fdf15a379d3be9dc0250a7
Sergio Lorente <slorente@campuscamarafp.com>
2021-05-27 11:47:00 +0200

    lofi de locos

---
+++ backend/public/js/Traspasos.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    // FETCHING DATA FROM JSON FILE
+    $.getJSON("http://localhost:3000/traspasos/get",
+        function (data) {
+            let student = '';
+
+
+            // ITERATING THROUGH OBJECTS
+            $.each(data, function (key, value) {
+                
+
+                //CONSTRUCTION OF ROWS HAVING
+                // DATA FROM JSON OBJECT
+                student += '<tr>';
+                student += '<td>' + value.Nickname + '</td>';
+
+                student += '<td>' + value.NombreEquipo + '</td>';
+
+                student += '<td>' +value.NombreRango + '</td>';
+
+                student += '</tr>';
+            });
+
+            //INSERTING ROWS INTO TABLE
+            $('#table').append(student);
+        });
+});
