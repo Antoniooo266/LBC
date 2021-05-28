e2f0a6d1d2f1ace5633927ec5641198ed907f1fa
Antonio <Antonio@LAPTOP-Q1IEL971>
2021-05-28 12:49:51 +0200

    mi niña bella cosita loca llego el momento de besar tu boca

--- backend/resutadosrouter.js
+++ backend/resutadosrouter.js
@@ -2,6 +2,18 @@ const connection = require("./config");
 var express = require("express");
 var router = express.Router();
 
+router.get('/get', (req, res)=>{
+  const sql = "SELECT * FROM view_partido_torneo";
+    connection.query(sql, (error, results) => {
+      if (error) throw error;
+      if (results.length > 0) {
+        res.json(results);
+      } else {
+        res.send("No hay resultados :(");
+      }
+    });
+})
+
 //----ADD RESULTS----
 
 router.post("/addresult", async (req, res) => {
