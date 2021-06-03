728fd906bf01ed7c7dc0b00daf7377dcdee94466
Antonio <Antonio@LAPTOP-Q1IEL971>
2021-06-03 09:12:12 +0200

    perreo pesau'

---
+++ backend/test/inicio.js
@@ -0,0 +1,31 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+chai.use(chaiHttp);
+const url= 'http://localhost:3000';
+
+describe('Inicio Usuario',()=>{
+    it('Register', function(done){
+        chai.request(url)
+        .post('/user/add')
+        .send({
+            Nickname:'pakito',
+            Contraseña:'pakito',
+            Fecha_Nac: '6969-06-31',
+            Correo: 'pakitooo@gmail.com',
+            País: 'España'
+        })
+        done();
+    })
+
+    it('Login', function(done){
+        chai.request(url)
+        .post('/user/logging')
+        .send({
+            firstname:'admin',
+            password:'admin'
+        })
+        done();
+    })
+})
