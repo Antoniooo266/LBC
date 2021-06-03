7ddccbf8361480e98c6d1e3061d6d10202b37cb5
abeja21 <you@example.com>
2021-06-03 09:24:45 +0200

    pueba unitaria de equipos

---
+++ backend/test/TEquipos.js
@@ -0,0 +1,34 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+chai.use(chaiHttp);
+const url= 'http://localhost:3000';
+
+describe('registrar a un equipo',()=>{
+    it('registrando a pepeteam',function(done){
+        chai.request(url)
+        .post('/equipo/add')
+        .send({
+            name:'pepeteam',
+            Capitan:'pepe',
+            joggo:'2',
+            NumJugadores:'10'
+    })
+    
+        done(); 
+    })
+})
+describe('modificar a un equipo',()=>{
+    it('modificando a pepe',function(done){
+        chai.request(url)
+        .post('/equipo/update')
+        .send({
+            id:"1",
+            equipo:'manolo',
+            joggo:'3',
+            Captain:'sergio'
+        })
+        done();
+    })
+})
